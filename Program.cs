06d847d30166f5100505bcddc54682a633457785
Rafael Corrêa de Melo <36901020+Lyem@users.noreply.github.com>
2020-06-03 18:41:06 -0300

    create jogo da memoria 1.0

---
+++ Program.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Threading;
+
+namespace JogoDaMemoria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.Clear();
+            float altura, largura;
+            altura = Console.WindowHeight;
+            largura = Console.WindowWidth;
+            metadel(largura/2+largura/3);
+            Console.Title = "Jogo da Memoria!!";
+            Console.WriteLine("Bem vindo ao Jogo da Memoria!!");
+            linha(largura);
+            Console.Write("\n");
+            int dificudade ,jogando = 0;
+            int[] num;
+            Random rand = new Random();
+            num = new int[5];
+            while (jogando == 0)
+            {
+                int acerto = 0, erros = 0,retorno = 0,level = 1;
+                Console.WriteLine("1-Fácil\n2-Medio\n3-Difícil");
+                Console.Write("Escolha a dificudade: ");
+                dificudade = Convert.ToInt32(Console.ReadLine());
+                if (dificudade == 1)
+                {
+                    altura = Console.WindowHeight;
+                    largura = Console.WindowWidth;
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.DarkBlue;
+                    Console.Clear();
+                    metadea(altura);
+                    metadel(largura);
+                    Console.WriteLine("Fácil");
+                    Thread.Sleep(3000);
+                    fini:
+                    for (int i = 0;i<5;i++)
+                    {
+                        Console.Clear();
+                        num[i] = rand.Next(1,10);
+                        metadea(altura);
+                        metadel(largura);
+                        Console.WriteLine(num[i]);
+                        if(level == 1)
+                        {
+                            Thread.Sleep(3000);
+                        }
+                        else if (level == 2)
+                        {
+                            Thread.Sleep(2000);
+                        }
+                        else if (level == 3)
+                        {
+                            Thread.Sleep(1000);
+                        }
+                    }
+                    for (int i = 0; i < 5; i++)
+                    {
+                        retorno = teste(num[i],i);
+                        acerto += retorno;
+                        if (retorno == 0)
+                        {
+                            erros += 1;
+                        }
+                    }
+                    Console.WriteLine("Acertou: " + acerto);
+                    Console.WriteLine("Errou: " + erros);
+                    Thread.Sleep(2000);
+                    if (acerto >= 2 && level<3)
+                    {
+                        level++;
+                        Console.WriteLine("Level" + level);
+                        Thread.Sleep(2000);
+                        goto fini;
+                    }
+                    else
+                    {
+                        metadea(altura);
+                        metadel(largura);
+                        Console.WriteLine("Game Over");
+                    }
+                }
+                else if (dificudade == 2)
+                {
+                    altura = Console.WindowHeight;
+                    largura = Console.WindowWidth;
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.DarkBlue;
+                    Console.Clear();
+                    metadea(altura);
+                    metadel(largura);
+                    Console.WriteLine("Medio");
+                    Thread.Sleep(3000);
+                mini:
+                    for (int i = 0; i < 5; i++)
+                    {
+                        Console.Clear();
+                        num[i] = rand.Next(1, 100);
+                        metadea(altura);
+                        metadel(largura);
+                        Console.WriteLine(num[i]);
+                        if (level == 1)
+                        {
+                            Thread.Sleep(2000);
+                        }
+                        else if (level == 2)
+                        {
+                            Thread.Sleep(1000);
+                        }
+                        else if (level == 3)
+                        {
+                            Thread.Sleep(1000);
+                        }
+                    }
+                    for (int i = 0; i < 5; i++)
+                    {
+                        retorno = teste(num[i], i);
+                        acerto += retorno;
+                        if (retorno == 0)
+                        {
+                            erros += 1;
+                        }
+                    }
+                    Console.WriteLine("Acertou: " + acerto);
+                    Console.WriteLine("Errou: " + erros);
+                    Thread.Sleep(2000);
+                    if (acerto >= 4 && level < 3)
+                    {
+                        level++;
+                        Console.WriteLine("Level" + level);
+                        Thread.Sleep(2000);
+                        goto mini;
+                    }
+                    else
+                    {
+                        metadea(altura);
+                        metadel(largura);
+                        Console.WriteLine("Game Over");
+                    }
+                }
+                else if (dificudade == 3)
+                {
+                    altura = Console.WindowHeight;
+                    largura = Console.WindowWidth;
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Clear();
+                    metadea(altura);
+                    metadel(largura);
+                    Console.WriteLine("Difícil");
+                    Thread.Sleep(3000);
+                dini:
+                    for (int i = 0; i < 5; i++)
+                    {
+                        Console.Clear();
+                        num[i] = rand.Next(1, 1000);
+                        metadea(altura);
+                        metadel(largura);
+                        Console.WriteLine(num[i]);
+                        if (level == 1)
+                        {
+                            Thread.Sleep(1000);
+                        }
+                        else if (level == 2)
+                        {
+                            Thread.Sleep(1000);
+                        }
+                        else if (level == 3)
+                        {
+                            Thread.Sleep(1000);
+                        }
+                    }
+                    for (int i = 0; i < 5; i++)
+                    {
+                        retorno = teste(num[i], i);
+                        acerto += retorno;
+                        if (retorno == 0)
+                        {
+                            erros += 1;
+                        }
+                    }
+                    Console.WriteLine("Acertou: " + acerto);
+                    Console.WriteLine("Errou: " + erros);
+                    Thread.Sleep(2000);
+                    if (acerto == 5 && level < 3)
+                    {
+                        level++;
+                        Console.WriteLine("Level" + level);
+                        Thread.Sleep(2000);
+                        goto dini;
+                    }
+                    else
+                    {
+                        metadea(altura);
+                        metadel(largura);
+                        Console.WriteLine("Game Over");
+                    }
+                }
+                Console.BackgroundColor = ConsoleColor.DarkBlue;
+            es:
+                Console.Clear();
+                Console.WriteLine("0-sim");
+                Console.WriteLine("1-não");
+                Console.Write("Jogar Novamente?: ");
+                try
+                {
+                    jogando = Convert.ToInt32(Console.ReadLine());
+                    Console.Clear();
+                }
+                catch
+                {
+                    Console.Clear();
+                    Console.WriteLine("Responda algo");
+                    Thread.Sleep(2000);
+                    goto es;
+                }
+
+            }
+        }
+
+        static void metadea(float a)
+        {
+            for (int i = 0;i<a/5;i++)
+            {
+                Console.WriteLine("\n");
+            }
+        }
+
+        static void metadel(float l)
+        {
+            for (float i = 0; i < l / 2; i++ )
+            {
+                Console.Write(" ");
+            }
+        }
+
+        static void linha(float l)
+        {
+            for (int i = 0; i < l; i++)
+            {
+                Console.Write("-");
+            }
+        }
+
+        static int teste(int n,int i)
+        {
+            Console.Clear();
+            int num,a = 0;
+            i += 1; 
+            Console.WriteLine("Escreva o " + i + " Numero");
+            num = Convert.ToInt32(Console.ReadLine());
+            if (n==num)
+            {
+                Console.WriteLine("Certo");
+                Thread.Sleep(2000);
+                a++;
+            }
+            else
+            {
+                Console.WriteLine("Errado");
+                Thread.Sleep(2000);
+            }
+            Console.Clear();
+            return a;
+        }
+    }
+}
